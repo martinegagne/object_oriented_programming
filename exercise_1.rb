028917e79966716a05b18f4e9f7f28c2f5c9dbb4
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-06 22:23:37 -0400

    update cat exercise

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,7 +1,7 @@
 #Exercise_1#
 class BankAccount
 
-  def initialize(balance, interest_rate)
+  def initialize(balance, interest_rate = 0.07)
     @balance = balance
     @interest_rate = interest_rate
   end
@@ -15,11 +15,11 @@ class BankAccount
   end
 
   def gain_interest(interest_deposit)
-    @interest_rate * @balance
+    @balance = (@balance * @interest_rate) + @balance
   end
 
 end
 
-my_bank_account = BankAccount.new(500, 0.05)
-my_bank_account.deposit (50)
-# p my_bank_account
+my_bank_account = BankAccount.new(500)
+# my_bank_account.deposit (50)
+# my_bank_account.withdraw (30)
--- exercise_2.rb
+++ exercise_2.rb
@@ -7,7 +7,21 @@ class Cat
     @meal_time = meal_time
   end
 
-  def 
+  def  eats_at
+    if meal_time >= 12
+      puts "#{meal_time}AM."
+    else
+      puts "#{meal_time - 12}PM."
+    end
+  end
+
+  def meow
+    puts "My name is #{@name} and I prefer to eat #{@preferred_food} at #{@meal_time}."
   end
 
 end
+
+m = Cat.new("Martin", "Kibble", 8)
+s = Cat.new("Sid", "Fish", 5)
+
+p m.meow
